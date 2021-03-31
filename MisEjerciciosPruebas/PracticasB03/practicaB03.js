7b1b9a16ec3fbb8cbee1ae1394a4f1bad4ad2117
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-03-31 21:32:16 +0200

    Update practicaB03.js

--- MisEjerciciosPruebas/PracticasB03/practicaB03.js
+++ MisEjerciciosPruebas/PracticasB03/practicaB03.js
@@ -1,5 +1,26 @@
+window.onload=function(){
 
-/*Resultado
-Los nombres introducidos son: nombre8, nombre5, nombre6, nombre1
-Los nombres ordenados son: nombre1,nombre5, nombre6, nombre8
-*/
+var arrayNombres=new Array();
+var nombre;
+var salida=1;
+var cont=0;
+
+do{//Mientras no se de a cancelar
+	nombre=prompt("Introduce tu nombre");
+	console.log(nombre);
+	if(nombre==null){//Si pulsa a cancelar
+		salida==0;//salimos del bucle
+	}
+	else{//Mientras que no pulse a cancelar se guardan los nombres
+		arrayNombres[cont]=nombre;
+		cont++;
+		console.log(cont);
+		console.log(arrayNombres);
+		console.log(arrayNombres.length);
+	}	
+}while(salida==1);
+
+document.getElementById("nombreIntroduccido").innerHTML="Los nombres introducidos son: " + arrayNombres;
+
+
+}//fin del window.onload
