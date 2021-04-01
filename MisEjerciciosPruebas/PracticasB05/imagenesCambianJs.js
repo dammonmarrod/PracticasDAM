f5e8f70f8acff843771691f54c91d9e315bf93ba
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-04-01 16:31:57 +0200

    Create imagenesCambianJs.js

---
+++ MisEjerciciosPruebas/PracticasB05/imagenesCambianJs.js
@@ -0,0 +1,12 @@
+window.onload=function(){
+
+	var superior=document.getElementById("superior");
+	var imagenesSuperior=superior.getElementsByTagName("td");
+	console.log(imagenesSuperior);
+
+
+	var inferior=document.getElementById("inferior");
+	var imagenesInferior=inferior.getElementsByTagName("td");
+	console.log(imagenesInferior);
+
+}//end window.onload
