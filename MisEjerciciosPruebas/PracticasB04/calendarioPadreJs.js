5b629c4c14ba6862c86addf9b281b38c2564af54
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-04-01 15:54:37 +0200

    Create calendarioPadreJs.js

---
+++ MisEjerciciosPruebas/PracticasB04/calendarioPadreJs.js
@@ -0,0 +1,5 @@
+//Se abre una nueva ventana
+//PROBLEMAS POSICION
+	var posicion="width=300px,height=400px,toolbar=no,resizable=no,location=no,left=screen.width,top=40";
+	window.open("Borrar2.html","",posicion);
+
