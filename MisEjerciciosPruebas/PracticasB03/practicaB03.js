66d063cd103dd1f2c31fb6451aa7877ad1c247c0
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-04-02 13:36:25 +0200

    Update practicaB03.js

--- MisEjerciciosPruebas/PracticasB03/practicaB03.js
+++ MisEjerciciosPruebas/PracticasB03/practicaB03.js
@@ -1,26 +1,41 @@
-window.onload=function(){
+window.onload=function(){//Para que se ejecute el script al cargarse la página
 
-var arrayNombres=new Array();
-var nombre;
-var salida=1;
-var cont=0;
+	var salida=1;
+	var arrayNombres=new Array();//creamos el array donde guardaremos los datos recogidos del usuario
+	var cont=0;
 
-do{//Mientras no se de a cancelar
-	nombre=prompt("Introduce tu nombre");
-	console.log(nombre);
-	if(nombre==null){//Si pulsa a cancelar
-		salida==0;//salimos del bucle
-	}
-	else{//Mientras que no pulse a cancelar se guardan los nombres
-		arrayNombres[cont]=nombre;
-		cont++;
-		console.log(cont);
-		console.log(arrayNombres);
-		console.log(arrayNombres.length);
-	}	
-}while(salida==1);
+	do{ //por lo menos tendremos que hacerlo una vez
+		nombre=prompt("Introduce un nombre para guardarlo ;.");
+		console.log("El resultado del prompt es: " + nombre);
 
-document.getElementById("nombreIntroduccido").innerHTML="Los nombres introducidos son: " + arrayNombres;
+
+		if(nombre!=null){//si ha dado a aceptar
+			if(!(isNaN (nombre))){//comprobamos si hay introducido un valor númerico
+					alert("El valor introducido es un número, y esto no es válido");
+			}
+			else{
+				arrayNombres[cont]=nombre;
+				console.log(arrayNombres);
+				console.log("valor contador antes incremento " + cont);
+				cont++;
+				console.log("valor contador despues incremento " + cont);
+				console.log("Longitud del array "+ arrayNombres.length);
+			}
+		}else{//Si le da a cancelar
+			if(cont==0){//comprobamos que al menos se haya guardado un nombre
+				alert("Debes introducir al menos un nombre.");
+			}else{
+				salida=0;
+			}
+			
+		}
+
+	}while(salida==1);
+	alert("He salido del bucle");
+	document.getElementById("nombreIntroducido").innerHTML="Los nombres introducidos son: " + arrayNombres;
+	arrayNombres.sort();
+	document.getElementById("nombreOrdenador").innerHTML="Los nombres ordenados son: " + arrayNombres;
+
 
 
 }//fin del window.onload
