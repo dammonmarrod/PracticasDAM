afe9bfa72352b6ab69bfbf56cf29dc822ee76584
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-04-02 17:34:57 +0200

    Update calendarioPadreJs.js

--- MisEjerciciosPruebas/PracticasB04/calendarioPadreJs.js
+++ MisEjerciciosPruebas/PracticasB04/calendarioPadreJs.js
@@ -1,5 +1,5 @@
 //Se abre una nueva ventana
-//PROBLEMAS POSICION
-	var posicion="width=300px,height=400px,toolbar=no,resizable=no,location=no,left=screen.width,top=40";
-	window.open("Borrar2.html","",posicion);
-
+	var ancho=screen.width;//Calculamos el ancho de la ventana
+	var left=screen.width - 300;//Le quitamos la anchura de la nueva ventana.
+	var posicion="width=300px,height=400px,toolbar=no,resizable=no,location=no,left=10000,top=0";
+	window.open("BorrarHija.html","",posicion);
