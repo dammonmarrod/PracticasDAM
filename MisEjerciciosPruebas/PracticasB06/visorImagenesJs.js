a05a2394c4fb7cdfc14e56eb8baaa87d3ed57d06
dammonmarrod <68920289+dammonmarrod@users.noreply.github.com>
2021-04-14 20:30:30 +0200

    Update visorImagenesJs.js

--- MisEjerciciosPruebas/PracticasB06/visorImagenesJs.js
+++ MisEjerciciosPruebas/PracticasB06/visorImagenesJs.js
@@ -14,7 +14,7 @@
 	//Al comenzar el boton anterior y primero tienen que estar quitados
 	btAnterior.disabled=true;
 	btPrimero.disabled=true;
-
+	//btAnterior.disabled="disabled"; Admite también esta forma
 
 	//Escuchadores
 	btSiguiente.addEventListener("click",pasarImagenSiguiente);
@@ -28,21 +28,24 @@
 	function pasarImagenSiguiente(){
 		btAnterior.disabled=false;//Recuperan su funcionalidad
 		btPrimero.disabled=false;//Recuperan su funcionalidad
-		if(posicion!=arrayImagenes.length-1){
-			++posicion;
+		
+		//Si lo ponemos encima del array; no afecta a la condición y entonces posicion es igual a arrayImagenes.length-1
+		if(posicion<arrayImagenes.length-1){
+			posicion++;//si lo ponemos antes 
 			console.log(posicion);
 			contenedorImagen.src=arrayImagenes[posicion];
-		}else{//Cuando la posición es igual a la longitud del array -1. Los botones pierden su funcionalidad
-			btUltimo.disabled=true;
-			btSiguiente.disabled=true;
+			if(posicion==arrayImagenes.length-1){//Cuando la posición es igual a la longitud del array -1. Los botones pierden su funcionalidad
+				btUltimo.disabled=true;
+				btSiguiente.disabled=true;
+			}  
 		}
 	}
 
 	function pasarImagenAnterior(){
 		btSiguiente.disabled=false; //Recuperan su funcionalidad
 		btUltimo.disabled=false; //Recuperan su funcionalidad
+		posicion--;
 		if(posicion!=0){
-			--posicion;
 			console.log(posicion);
 			contenedorImagen.src=arrayImagenes[posicion];
 		}else{
@@ -54,10 +57,11 @@
 	function pasarImagenUltimo(){
 		btAnterior.disabled=false;
 		btPrimero.disabled=false;
-		contenedorImagen.src=arrayImagenes[arrayImagenes.length-1];
+		posicion=arrayImagenes.length-1;//El valor de la posición ahora se corresponderá a la imagen ultima del array
+		contenedorImagen.src=arrayImagenes[posicion];
 		btSiguiente.disabled=true;
 		btUltimo.disabled=true;
-		posicion=arrayImagenes.length-1;//El valor de la posición ahora se corresponderá a la imagen ultima del array
+		
 
 
 	}
@@ -65,9 +69,10 @@
 	function pasarImagenPrimero(){
 		btSiguiente.disabled=false;
 		btUltimo.disabled=false;
-		contenedorImagen.src=arrayImagenes[0];
+		posicion=0;//La posición corresponderá a la imagen primera del array
+		contenedorImagen.src=arrayImagenes[posicion];
 		btAnterior.disabled=true;
 		btPrimero.disabled=true;
-		posicion=0;//La posición corresponderá a la imagen primera del array
+		
 		console.log(posicion);
 	}
